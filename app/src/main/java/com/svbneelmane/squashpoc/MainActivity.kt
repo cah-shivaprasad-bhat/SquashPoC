a6366fb7d5df7cafa1da07f255514f4f71078a6f
Shivaprasad Bhat <82654063+cah-shivaprasad-bhat@users.noreply.github.com>
2025-02-11 11:57:08 +0530

    PR : Squash check (#1): Merged 2 commit into 1

--- app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
+++ app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.svbneelmane.squashpoc.ui.theme.SquashPoCTheme
 
 class MainActivity : ComponentActivity() {
+    // Commit 1: Adding commit 1
+    // Commit 2: Adding commit 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
