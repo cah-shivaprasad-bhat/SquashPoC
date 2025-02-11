630a69e11b347912d4d91ad5d6732388d07c74f1
cah-shivaprasad-bhat <shivaprasad.bhat@cardinalhealth.com>
2025-02-11 14:25:08 +0530

    Squash 5

--- app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
+++ app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
@@ -13,11 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.svbneelmane.squashpoc.ui.theme.SquashPoCTheme
 
+// Squash 2
+// Squash 4
 class MainActivity : ComponentActivity() {
     // Commit 1: Adding commit 1
     // Commit 2: Adding commit 2
     // Commit 3: Adding commit 3
     // Commit 4: Adding commit 4
+    // Squash 5: Squash 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -47,7 +50,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+// Squash 1
+// Squash 2
+// Squash 3
+// Squash 4
+ @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SquashPoCTheme {
