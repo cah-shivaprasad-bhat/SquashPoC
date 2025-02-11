d5ec26b92f067a6ff25bd1834608b8105ef88bfd
cah-shivaprasad-bhat <shivaprasad.bhat@cardinalhealth.com>
2025-02-11 11:54:53 +0530

    Commit 1

--- app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
+++ app/src/main/java/com/svbneelmane/squashpoc/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.svbneelmane.squashpoc.ui.theme.SquashPoCTheme
 
 class MainActivity : ComponentActivity() {
+    // Commit 1: Adding commit 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
